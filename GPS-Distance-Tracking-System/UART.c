552f009294f82d2dd460c26c2a4e0e88318d3e17
Mahmoud Hossam <mahmoudagouz58@gmail.com>
2024-05-02 19:26:45 +0300

    Add funtion for init_uart0 (PC) / funtion fot init_uart1(GPS)

---
+++ GPS-Distance-Tracking-System/UART.c
@@ -0,0 +1,41 @@
+
+#include "TIVA_HEADERS.h"
+#include <string.h>
+
+void UART_Init_pc(void) // funtion for init uart0
+{
+
+	SYSCTL_RCGCUART_R |= SYSCTL_RCGCUART_R0; // activate uart 0
+	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R0; // activate port A
+
+	UART0_CTL_R &= ~UART_CTL_UARTEN; // disable uart0
+
+	UART0_IBRD_R = 0X208;											// IBRD=INT(80000000/(9600*16)) =520
+	UART0_FBRD_R = 0X35;											// FBRD=ROUND(0.833333*64) =53
+	UART0_LCRH_R = (UART_LCRH_FEN | UART_LCRH_WLEN_8);				// 1110000   ENABLE FIFO + DATA IS 8 BITS
+	UART0_CTL_R |= (UART_CTL_RXE | UART_CTL_TXE | UART_CTL_UARTEN); // enable RXE, TXE UART
+	GPIO_PORTA_AFSEL_R |= 0X03;
+	GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R & 0XFFFFFF00) | (GPIO_PCTL_PA0_U0RX | GPIO_PCTL_PA1_U0TX);
+	GPIO_PORTA_DEN_R |= 0X03;	 // enable digital on PA0,PA1
+	GPIO_PORTA_AMSEL_R &= ~0X03; // disable analog on PA0,PA1
+}
+
+
+
+void UART_Init_gps(void) // funtion for init uart1
+{
+
+	SYSCTL_RCGCUART_R |= SYSCTL_RCGCUART_R1; // activate uart 1 //? port B
+	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R1; // activate port B
+
+	UART1_CTL_R &= ~UART_CTL_UARTEN; // disable uart1
+
+	UART1_IBRD_R = 0X208;										   // IBRD=INT(80000000/(9600*16)) =520 //todo check
+	UART1_FBRD_R = 0X35;										   // FBRD=ROUND(0.833333*64) =53
+	UART1_LCRH_R = (UART_LCRH_PEN | UART_LCRH_WLEN_8);			   // 1110000   ENABLE FIFO + DATA IS 8 BITS
+	UART1_CTL_R |= (UART_CTL_TXE | UART_CTL_RXE | UART_CTL_UARTEN); // enable RXE, TXE UART
+	GPIO_PORTB_AFSEL_R |= 0X03;
+	GPIO_PORTB_PCTL_R = (GPIO_PORTB_PCTL_R & 0XFFFFFF00) + (GPIO_PCTL_PB0_U1RX | GPIO_PCTL_PB1_U1TX);
+	GPIO_PORTB_DEN_R |= 0X03;	 // enable digital on PB0,PB1
+	GPIO_PORTB_AMSEL_R &= ~0X03; // disable analog on PB0,PB1
+}
---
+++ GPS-Distance-Tracking-System/UART.h
@@ -0,0 +1,13 @@
+#ifndef UART.h
+#define UART.h
+
+void UART_Init_pc(void); // init_uart_pc
+void UART_Init_gps(void);// init_uart_pc
+
+
+
+
+
+
+
+#endif
