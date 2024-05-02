c53278836d53870d740e9f6faebe39dfe378a219
Omar Hesham <omarhisham844@gmail.com>
2024-05-02 20:19:28 +0300

    adding some functions in GPIO.c

--- GPS-Distance-Tracking-System/GPIO.c
+++ GPS-Distance-Tracking-System/GPIO.c
@@ -1,6 +1,6 @@
 #include "TIVA_HEADERS.h"
 #include GPIO.h
- 
+
 void SW_Init(void){
 	GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;
 	GPIO_PORTF_CR_R |= SW_ALL_MASK; //allow changes for port 1,2,3 for portf
@@ -12,19 +12,16 @@ void SW_Init(void){
 	GPIO_PORTF_DEN_R |=SW_ALL_MASK;
 	GPIO_PORTF_PUR_R  =SW_ALL_MASK;
 }
- 
+
 unsigned char SW2_Input(void){
 	return GPIO_PORTF_DATA_R & SW2_MASK;
 }
- 
 unsigned char SW1_Input(void){
 	return GPIO_PORTF_DATA_R & SW1_MASK;
 }
- 
 unsigned char switch1On(void){		// return true if switch1 is pressed on
 	return ((~GPIO_PORTF_DATA_R & 0x10) == 0x10);
 }
- 
 unsigned char switch2On(void){
 	return ((~GPIO_PORTF_DATA_R & 0x01) == 0x01);
 }
