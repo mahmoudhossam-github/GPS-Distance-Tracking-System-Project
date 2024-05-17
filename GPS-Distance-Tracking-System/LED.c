34f1dadecfe6af1494944d6cf796f88a2272803a
mohamedelhawary2 <mohamedelhawary3@yahoo.com>
2024-05-17 14:44:52 +0300

    Updated led config
    Add SysTick functions and switches functions

--- GPS-Distance-Tracking-System/LED.c
+++ GPS-Distance-Tracking-System/LED.c
@@ -1,7 +1,8 @@
 #include "TIVA_HEADERS.h"
 #include "LED.h"
 
-void turn_On_LED(unsigned char data) {
+void turn_On_LED(unsigned char data)
+{
 	GPIO_PORTF_DATA_R |= data;
 }
 
@@ -18,24 +19,74 @@ void LED_Init(void) {
 	GPIO_PORTF_DATA_R &= ~LED_MASK; 										// Clear Data
 }
 
-void turn_Off_LED(unsigned char data){
-	GPIO_PORTF_DATA_R &= ~data;
+void SW_Init(void) {
+	GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;
+	GPIO_PORTF_CR_R |= SW_MASK; //allow changes for port 1,2,3 for portf
+	GPIO_PORTF_AMSEL_R &= ~SW_MASK;
+	GPIO_PORTF_AFSEL_R &= ~SW_MASK;
+	GPIO_PORTF_PCTL_R &= ~GPIO_PCTL_PF0_M; //clear bits in PCTL
+	GPIO_PORTF_PCTL_R &= ~GPIO_PCTL_PF4_M;
+	GPIO_PORTF_DIR_R &= ~SW_MASK;
+	GPIO_PORTF_DEN_R |= SW_MASK;
+	GPIO_PORTF_PUR_R = SW_MASK;
 }
 
-unsigned char getData_LED(void){
+void turn_Off_LED(unsigned char data)
+{
+	GPIO_PORTF_DATA_R &= ~data;
+}
+void LED_Reset(void)
+{
+	GPIO_PORTF_DATA_R &= ~LED_MASK;
+}
+
+void LED_Wait(void)
+{
+	GPIO_PORTF_DATA_R |= (Red | Green);
+}
+unsigned char getData_LED(void)
+{
 	return GPIO_PORTF_DATA_R & LED_MASK;
 }
 
-void SysTick_Init(void){
-  NVIC_ST_CTRL_R = 0;
-  NVIC_ST_RELOAD_R = 0x00FFFFFF;
-  NVIC_ST_CURRENT_R = 0;
-  NVIC_ST_CTRL_R = 0x5;
+void SysTick_Init(void)
+{
+	NVIC_ST_CTRL_R = 0;
+	NVIC_ST_RELOAD_R = 0x00FFFFFF;
+	NVIC_ST_CURRENT_R = 0;
+	NVIC_ST_CTRL_R = 0x5;
 }
 
-void SysTick_Delay10ms(void){
-  NVIC_ST_RELOAD_R = 159999;
-  NVIC_ST_CURRENT_R = 0;
-  while((NVIC_ST_CTRL_R&0x00010000) == 0) {};
+void SysTick_Delay10ms(void)
+{
+	NVIC_ST_RELOAD_R = 159999;
+	NVIC_ST_CURRENT_R = 0;
+	while ((NVIC_ST_CTRL_R & 0x00010000) == 0) {};
+}
+
+
+void Delay_in_seconds(unsigned char seconds)
+{
+	int i;
+	for (i = 0; i < 100 * seconds; i++) SysTick_Delay10ms();
+}
+
+unsigned char SW2_Input(void)
+{
+	return GPIO_PORTF_DATA_R & SW2_MASK;
+}
+unsigned char SW1_Input(void)
+{
+	return GPIO_PORTF_DATA_R & SW1_MASK;
+}
+
+unsigned char SW1_ON(void)
+{
+	return (!(GPIO_PORTF_DATA_R & SW1_MASK));
+}
+
+unsigned char SW2_ON(void)
+{
+	return (!(GPIO_PORTF_DATA_R & SW2_MASK));
 }
 
--- GPS-Distance-Tracking-System/LED.h
+++ GPS-Distance-Tracking-System/LED.h
@@ -1,21 +1,68 @@
 #ifndef _LED_H_
 #define _LED_H_
 
-#define LED_MASK 0xE
+
+/* ------- Important Notes for LED colors ------- */
+
+// Red -> ERROR
+// Green -> DONE
+// Blue -> In Progress
+// Yellow -> Waiting for interaction OR Waiting for GPS
+
+/* ---------------------------------------------- */
+#define LED_MASK 0xE // 0b01110
+#define PF_MASK 0x1F // 0b11111
+#define SW_MASK 0x11
+#define SW1_MASK  0x10
+#define SW2_MASK	0x01
+
 #define Red 0x02
 #define Blue 0x04
 #define Green 0x08
 
-void LED_Init(void);	// LED Initialization
 
-void turn_On_LED(unsigned char data); // Turns on Red, Blue or Green LED
+void LED_Init(void);
+// LED Initialization
 
-void turn_Off_LED(unsigned char data); // Turns off Red, Blue or Green LED
+void SW_Init(void);
 
-unsigned char getData_LED(void); // Read current LED status (PF321 - G B R)
+void turn_On_LED(unsigned char data);
+// Turns on Red, Blue or Green LED
 
-void SysTick_Init(void);  // Timer Initialization
- 
-void SysTick_Delay10ms(void);  // 10ms Delay (Clock = 16 MHz)
+void turn_Off_LED(unsigned char data);
+// Turns off Red, Blue or Green LED
+
+void LED_Reset(void);
+// Turns off ALL LEDs
+
+
+void LED_Wait(void);
+// Turns on Yellow (Wait Indicator)
+
+unsigned char getData_LED(void);
+// Read current LED status (PF321 - G B R)
+
+unsigned char SW1_Input(void);
+// Reads input from SW1
+
+unsigned char SW2_Input(void);
+// Reads input from SW2
+
+unsigned char SW1_ON(void);
+// Check if SW1 is ON
+
+unsigned char SW2_ON(void);
+// Check if SW2 is ON
+
+void SysTick_Init(void);
+// Timer Initialization
+
+void SysTick_Delay10ms(void);
+// 10ms Delay (Clock = 16 MHz)
+
+void Delay_in_seconds(unsigned char seconds);
+// Utility Function for whatever seconds delay!
+
+
 
 #endif
