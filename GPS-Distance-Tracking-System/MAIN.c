c6e90e3e1a40909a2a6b1668e1d47e2fef519c0d
Mahmoud Hossam <mahmoudagouz58@gmail.com>
2024-05-17 15:07:12 +0300

    upload funtion for Gps_read / funtion for covert_to_degree / funtion for convert_to_rad

--- GPS-Distance-Tracking-System/MAIN.c
+++ GPS-Distance-Tracking-System/MAIN.c
@@ -2,7 +2,7 @@
 #include "LED.h"
 #include "GPIO.h"
 #include "UART.h"
-
+#define M_PI 3.14159265358979323846
 //------------------------//
 //--- Global Variables ---//
 //------------------------//
@@ -12,6 +12,73 @@ unsigned char button_in2; // Variable to receive input from SW2
 unsigned char button_prev1 = 0; // Used for FALLING EDGE Condition of SW1
 unsigned int distance = 50 ; // Distance variable
 unsigned char LED_current; // Variable to get current LED data
+char GPS_logName[] = "$GPGLL,"; 
+char GPS_COUNTER;
+char GPS[80];   
+
+
+//------------------------//
+//--- GPS_READ_FUNCTION---//
+//------------------------//
+char GPS_logName[] = "$GPGLL,"; 
+char GPS_COUNTER;
+char GPS[80];   
+
+
+void GPS_READ(void)  //we will use GPGLL
+{
+	int k;
+	int r;	
+	char i;
+    char RECIEVED_CHAR; 
+    char flag = 1; 
+    do 
+    { 
+        flag = 1; 
+        
+        for (i = 0; i < 7; i++) //"$GPGLL,"
+        { 
+ 
+            if (UART_InChar_gps() != GPS_logName[i]) 
+            { 
+                flag = 0; // break and exit from for loop 
+                break; 
+            } 
+        } 
+    } while (flag == 0);// i checked recieved output true or not  will continue checking untill the desired LOG arrives
+ 
+   strcpy(GPS, ""); // INIT GPS ARRAY 
+   GPS_COUNTER =0;
+do{ 
+     
+    RECIEVED_CHAR=UART_InChar_gps(); 
+     
+    GPS[GPS_COUNTER++]=RECIEVED_CHAR;  //now we have a char array called GPS  29.9756000,N,31.2372000,E,182908.00,A,A*
+	
+} while(RECIEVED_CHAR!='*'); 
+
+}
+
+//----------------------------------------//
+//--- CONVERT_T0_DEGREE_ANGLE_FUNTION  ---//
+//---------------------------------------//
+
+float convert_to_degree_angle(float angle) 
+{ 
+    int degree = (int)angle / 100; 
+    float minutes = angle - (float)degree * 100; 
+    return (degree + (minutes / 60)); 
+} 
+
+//------------------------------------//
+//--- CONVERT_T0_RAD_ANGLE_FUNTION ---//
+//------------------------------------//
+float convert_to_rad_angle(float angle) 
+{ 
+ 
+    return angle * M_PI / 180; 
+}
+
 
 
 //----------------------------------------------------------------------//
