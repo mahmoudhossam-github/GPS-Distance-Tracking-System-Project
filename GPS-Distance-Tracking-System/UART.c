39d68ec9c305590f034844bf4a4837abe6f7a5d4
Mahmoud Hossam <mahmoudagouz58@gmail.com>
2024-05-17 15:39:32 +0300

    update UART

--- GPS-Distance-Tracking-System/UART.c
+++ GPS-Distance-Tracking-System/UART.c
@@ -1,17 +1,20 @@
-
-#include "TIVA_HEADERS.h"
+#include "TIVA_HEADERS.h" 
 #include "UART.h"
 #include <string.h>
 
 
 void UART_Init_pc(void) // funtion for init uart0
 {
-
 	SYSCTL_RCGCUART_R |= SYSCTL_RCGCUART_R0; // activate uart 0
+	while((SYSCTL_PRUART_R & SYSCTL_PRUART_R0==0)){};//delay uart0
+   
+
 	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R0; // activate port A
+	while((SYSCTL_PRGPIO_R & SYSCTL_PRGPIO_R0)==0){};//delay port A
+
+	
 
 	UART0_CTL_R &= ~UART_CTL_UARTEN; // disable uart0
-
 	UART0_IBRD_R = 0X68;											// IBRD=INT(16*10^6/(9600*16)) =104
 	UART0_FBRD_R = 0X0B;											// FBRD=ROUND(0.166666*64) =11
 	UART0_LCRH_R = (UART_LCRH_FEN | UART_LCRH_WLEN_8);				// 1110000   ENABLE FIFO + DATA IS 8 BITS
@@ -21,6 +24,7 @@ void UART_Init_pc(void) // funtion for init uart0
 	GPIO_PORTA_DEN_R |= 0X03;	 // enable digital on PA0,PA1
 	GPIO_PORTA_AMSEL_R &= ~0X03; // disable analog on PA0,PA1
 }
+
 
 char UART_InChar_pc() // read uart data
 {
@@ -35,40 +39,39 @@ void UART_OutChar_pc(char data) // write uart data
 	UART0_DR_R = data; // we just want the 8 bits
 }
 
-
-
-void UART_Init_gps(void)
+void UART_Init_gps(void)// for portd uart 2
 {
-
-	SYSCTL_RCGCUART_R |= SYSCTL_RCGCUART_R1; // activate uart 1 //? port B
-	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R1; // activate port B
-
-	UART1_CTL_R &= ~UART_CTL_UARTEN; // disable uart1
-
-	UART1_IBRD_R = 0X68;										   // IBRD=INT(16*10^6/(9600*16)) =104 
-	UART1_FBRD_R = 0X0B;										   // FBRD=ROUND(0.166666*64) =11
-	UART1_LCRH_R = (UART_LCRH_PEN | UART_LCRH_WLEN_8);			   // 1110000   ENABLE FIFO + DATA IS 8 BITS
-	UART1_CTL_R |= (UART_CTL_TXE | UART_CTL_RXE | UART_CTL_UARTEN); // enable RXE, TXE UART
-	GPIO_PORTB_AFSEL_R |= 0X03;
-	GPIO_PORTB_PCTL_R = (GPIO_PORTB_PCTL_R & 0XFFFFFF00) + (GPIO_PCTL_PB0_U1RX | GPIO_PCTL_PB1_U1TX);
-	GPIO_PORTB_DEN_R |= 0X03;	 // enable digital on PB0,PB1
-	GPIO_PORTB_AMSEL_R &= ~0X03; // disable analog on PB0,PB1
+	SYSCTL_RCGCUART_R |= SYSCTL_RCGCUART_R2; // activate uart 2 
+    while(SYSCTL_PRUART_R & SYSCTL_PRUART_R2==0);//delay uart2
+    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R3; // activate port D
+	while(SYSCTL_PRGPIO_R & SYSCTL_PRGPIO_R3==0){};//delay port D
+	UART2_CTL_R &= ~UART_CTL_UARTEN; // disable uart2
+	UART2_IBRD_R = 0X68;										   // IBRD=INT(16*10^6/(9600*16)) =104 
+	UART2_FBRD_R = 0X0B;										   // FBRD=ROUND(0.166666*64) =11
+	UART2_LCRH_R = (UART_LCRH_FEN | UART_LCRH_WLEN_8);			   // 1110000   ENABLE FIFO + DATA IS 8 BITS
+	UART2_CTL_R |= (UART_CTL_TXE | UART_CTL_RXE | UART_CTL_UARTEN); // enable RXE, TXE UART
+	GPIO_PORTD_LOCK_R=GPIO_LOCK_KEY;
+	GPIO_PORTD_CR_R|=0xC0;
+	GPIO_PORTD_AFSEL_R |= ((1 << 6) | (1 << 7)); // Enable alternate function on PD6 and PD7
+    GPIO_PORTD_PCTL_R = (GPIO_PORTD_PCTL_R & 0x00FFFFFF) + (GPIO_PCTL_PD6_U2RX | GPIO_PCTL_PD7_U2TX); // Select UART alternate functions for PD6 and PD7
+    GPIO_PORTD_DEN_R |= ((1 << 6) | (1 << 7)); // Enable digital I/O on PD6 and PD7
+    GPIO_PORTD_AMSEL_R &= ~((1 << 6) | (1 << 7)); // Disable analog functionality on PD6 and PD7
 }
+
 
 char UART_InChar_gps() // read uart data
 {
-	while ((UART1_FR_R & UART_FR_RXFE) != 0); // while fifo is empty>>dont read
-	return (char)UART1_DR_R;  // we just want the 8 bits
+	while ((UART2_FR_R & UART_FR_RXFE) != 0); // while fifo is empty>>dont read
+	return (char)UART2_DR_R;  // we just want the 8 bits
 }
 
 void UART_OutChar_gps(char data) // write uart data
 {
 
-	while ((UART1_FR_R & UART_FR_TXFF) != 0); // while fifo is full>>dont write
-	UART1_DR_R = data; // we just want the 8 bits
+	while ((UART2_FR_R & UART_FR_TXFF) != 0); // while fifo is full>>dont write
+	UART2_DR_R = data; // we just want the 8 bits
 }
 
-// function//
 
 
 
